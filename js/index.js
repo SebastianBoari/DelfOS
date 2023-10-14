410e36c7e6221b6c4677f3f7f860efe421503594
sebastian <luciosebastianboari@gmail.com>
2023-10-14 02:29:13 -0300

    refactor[footer]: styles and clasees to bem

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const footerForm = document.getElementById('footer_form')
+
+    footerForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+    })
+})
